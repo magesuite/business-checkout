92ee5e7f069d345a3ceba084eaa49c62c4f81e47
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2020-08-21 16:38:53 +0200

    Compatibility with PHPUNIT 9

--- Test/Integration/Plugin/AddCustomerTypeToAddressFormTest.php
+++ Test/Integration/Plugin/AddCustomerTypeToAddressFormTest.php
@@ -43,7 +43,7 @@ class AddCustomerTypeToAddressFormTest extends \Magento\TestFramework\TestCase\A
      */
     protected $session;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
